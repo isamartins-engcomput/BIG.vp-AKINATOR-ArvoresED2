ea285ebae7a46c29a6f619b02b9158279ccfb764
brunofelix14 <brunosecundaria2222@gmail.com>
2026-04-11 16:34:45 -0300

    adição da musica tema

--- morpheus/program/src/main.c
+++ morpheus/program/src/main.c
@@ -22,7 +22,11 @@ typedef struct No
 
 int testa_no(struct No * no)
 {
-    if(no == NULL) return 0;
+    if(no == NULL) 
+    {
+      return 0;
+    }
+
     return 1;
 }
 
@@ -31,7 +35,11 @@ void preenche_arvore(struct No ** raiz,  char pergunta[][51], int pergunta_atual
     if(pergunta_atual < total_perguntas)
     { 
         *raiz = malloc(sizeof(No));
-        if(!testa_no(*raiz)) { printf("Deu gangrena :D\n"); return; }
+        if(!testa_no(*raiz)) 
+        { 
+          printf("Deu gangrena :D\n"); 
+          return; 
+        }
 
         (*raiz)->eh_personagem = 'N'; 
         strcpy((*raiz)->conteudo, pergunta[pergunta_atual]);
@@ -44,7 +52,12 @@ void preenche_arvore(struct No ** raiz,  char pergunta[][51], int pergunta_atual
     else
     {
         *raiz = malloc(sizeof(No));
-        if(!testa_no(*raiz)) { printf("Deu gangrena :D\n"); return; }
+        if(!testa_no(*raiz)) 
+        { 
+          printf("Deu gangrena :D\n"); 
+          return; 
+        }
+
         (*raiz)->eh_personagem = 'S';
         strcpy((*raiz)->conteudo, "Ninguém (Banco Vazio)"); 
         (*raiz)->sim = NULL;
@@ -63,13 +76,27 @@ void processa_linha(struct No ** raiz, char * linha)
     {
         if(strlen(token) == 1 && (token[0] == 'S' || token[0] == 'N' || token[0] == 's' || token[0] == 'n'))
         {
-            if(atual == NULL) break;
-            if(token[0] == 'S' || token[0] == 's') atual = atual->sim;
-            else atual = atual->nao;
+            if(atual == NULL)
+            {
+              return;
+            }
+ 
+            if(token[0] == 'S' || token[0] == 's') 
+            {
+              atual = atual->sim;
+            }
+            else
+            {
+              atual = atual->nao;
+            } 
         }
         else
         {
-            if(strlen(token) > 2) strcpy(nome, token);
+            if(strlen(token) > 2)
+            {
+               strcpy(nome, token);
+            }
+
         }
         token = strtok(NULL, ",");
     }
@@ -81,39 +108,43 @@ void processa_linha(struct No ** raiz, char * linha)
 
 void preenche_folhas(struct No ** raiz)
 {
-    FILE * dataSet;
-    char filename[] = "morpheus/dataBase/dataSet.txt";
-    dataSet = fopen(filename, "r");
+    FILE * filmes_e_series;
+    char filename[] = "filmes_e_series.txt"; 
+    filmes_e_series = fopen(filename, "r");
     
-    if(dataSet == NULL)
+    if(filmes_e_series == NULL)
     {
-        dataSet = fopen(filename, "w");
+        filmes_e_series = fopen(filename, "w");
         // ATENÇÃO AQUI: Agora são 6 vírgulas (6 respostas) antes do nome do filme!
-        fprintf(dataSet, "S,N,N,N,N,N,Interestelar\n"); 
-        fprintf(dataSet, "S,S,N,S,S,N,Scott Pilgrim contra o Mundo\n");        
-        fprintf(dataSet, "S,S,S,N,N,N,Tropa de Elite\n");    
-        fprintf(dataSet, "N,N,S,S,S,N,Todas as Flores\n");  
-        fprintf(dataSet, "S,N,S,S,S,N,Lisbela e o Prisioneiro\n"); 
-        fprintf(dataSet, "S,S,N,S,N,S,Homem-Aranha no Aranhaverso\n"); // Exemplo respondendo SIM para animação
-        fclose(dataSet);
-        dataSet = fopen(filename, "r"); 
+        fprintf(filmes_e_series, "S,N,N,N,N,N,Interestelar\n"); 
+        fprintf(filmes_e_series, "S,S,N,S,S,N,Scott Pilgrim contra o Mundo\n");        
+        fprintf(filmes_e_series, "S,S,S,N,N,N,Tropa de Elite\n");    
+        fprintf(filmes_e_series, "N,N,S,S,S,N,Todas as Flores\n");  
+        fprintf(filmes_e_series, "S,N,S,S,S,N,Lisbela e o Prisioneiro\n"); 
+        fprintf(filmes_e_series, "S,S,N,S,N,S,Homem-Aranha no Aranhaverso\n"); // Exemplo respondendo SIM para animação
+        fclose(filmes_e_series);
+        filmes_e_series = fopen(filename, "r"); 
     }
 
-    if(dataSet != NULL)
+    if(filmes_e_series != NULL)
     {
         char linha[256];
-        while(fgets(linha,sizeof(linha),dataSet) != NULL)
+        while(fgets(linha,sizeof(linha),filmes_e_series) != NULL)
         {
             linha[strcspn(linha, "\n")] = 0; 
             processa_linha(raiz, linha);
         }
-        fclose(dataSet); 
+        fclose(filmes_e_series); 
     }
 }
 
 void libera_arvore(No * no)
 {
-    if (no == NULL) return;
+    if (no == NULL)
+    {
+      return;
+    } 
+
     libera_arvore(no->sim);
     libera_arvore(no->nao);
     free(no);
@@ -126,10 +157,13 @@ bool DrawButton(Rectangle rec, const char *text, int fontSize, Color color, Colo
     bool clicked = false;
     Color currentBtnColor = color;
 
-    if (CheckCollisionPointRec(mousePoint, rec))
+    if(CheckCollisionPointRec(mousePoint, rec))
     {
         currentBtnColor = ColorBrightness(color, -0.15f); 
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) clicked = true;
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+           clicked = true;
+        }
     }
 
     DrawRectangleRounded((Rectangle){ rec.x + 4, rec.y + 4, rec.width, rec.height }, 0.2f, 8, Fade(PALETTE_DARKRED, 0.2f));
@@ -148,6 +182,7 @@ int main(void)
     const int screenHeight = 700;
     
     InitWindow(screenWidth, screenHeight, "MORPHEUS");
+    InitAudioDevice();
     SetTargetFPS(60);
 
     // Array aumentado para tamanho 6 e aspas corrigidas na nova pergunta
@@ -162,13 +197,15 @@ int main(void)
     
     // Array aumentado para armazenar as 6 imagens (0 a 5)
     Texture2D texturas_perguntas[6];
+    Music tema = LoadMusicStream("morpheus/program/include/song.mp3");
+    PlayMusicStream(tema);
     texturas_perguntas[0] = LoadTexture("morpheus/program/include/pergunta1.png");
     texturas_perguntas[1] = LoadTexture("morpheus/program/include/pergunta2.png");
     texturas_perguntas[2] = LoadTexture("morpheus/program/include/pergunta3.png");
     texturas_perguntas[3] = LoadTexture("morpheus/program/include/pergunta4.png");
     texturas_perguntas[4] = LoadTexture("morpheus/program/include/pergunta5.png");
     texturas_perguntas[5] = LoadTexture("morpheus/program/include/pergunta6.png"); 
-
+ 
     No *noAtual = raiz;
     int pergunta_contador = 0;
     bool fim_de_jogo = false;
@@ -176,17 +213,22 @@ int main(void)
 
     float escala = 0.25f;
 
-    while (!WindowShouldClose())
+    while(!WindowShouldClose())
     {
-        if (tela_inicial)
+        UpdateMusicStream(tema);
+
+        if(tela_inicial)
         {
             Rectangle recJogar = { screenWidth/2 - 125, 580, 250, 60 };
-            if (DrawButton(recJogar, "Jogar", 24, PALETTE_YELLOW, PALETTE_DARKRED)) tela_inicial = false;
+            if(DrawButton(recJogar, "Jogar", 24, PALETTE_YELLOW, PALETTE_DARKRED))
+            {
+                tela_inicial = false;
+            } 
         }
-        else if (fim_de_jogo)
+        else if(fim_de_jogo)
         {
             Rectangle recRestart = { screenWidth/2 - 125, 580, 250, 60 };
-            if (DrawButton(recRestart, "Jogar Novamente", 24, PALETTE_YELLOW, PALETTE_DARKRED)) 
+            if(DrawButton(recRestart, "Jogar Novamente", 24, PALETTE_YELLOW, PALETTE_DARKRED)) 
             {
                 noAtual = raiz;
                 pergunta_contador = 0;
@@ -194,17 +236,17 @@ int main(void)
                 tela_inicial = true;
             }
         }
-        else if (noAtual->eh_personagem == 'N') 
+        else if(noAtual->eh_personagem == 'N') 
         {
             Rectangle recSim = { screenWidth/2 - 200, 560, 150, 70 };
             Rectangle recNao = { screenWidth/2 + 50, 560, 150, 70 };
 
-            if (DrawButton(recSim, "SIM", 32, PALETTE_BLUE, PALETTE_BG)) 
+            if(DrawButton(recSim, "SIM", 32, PALETTE_BLUE, PALETTE_BG)) 
             {
                 noAtual = noAtual->sim;
                 pergunta_contador++;
             }
-            else if (DrawButton(recNao, "NÃO", 32, PALETTE_RED, PALETTE_BG)) 
+            else if(DrawButton(recNao, "NÃO", 32, PALETTE_RED, PALETTE_BG)) 
             {
                 noAtual = noAtual->nao;
                 pergunta_contador++;
@@ -218,14 +260,15 @@ int main(void)
         BeginDrawing();
         ClearBackground(BLACK);
 
-        if (tela_inicial)
+        if(tela_inicial)
         {
             int textY = 120;
             DrawText("Olá, eu sou MORPHEUS!", screenWidth/2 - MeasureText("Olá, eu sou MORPHEUS!", 34)/2, textY, 34, PALETTE_BLUE);
             const char* subtexto = "Pense em um filme ou uma série e eu irei tentar adivinhar qual é.";
             DrawText(subtexto, screenWidth/2 - MeasureText(subtexto, 20)/2, textY + 60, 20, WHITE);
 
-            if (textura_morpheus.id > 0) {
+            if(textura_morpheus.id > 0) 
+            {
                 float imgLargura = textura_morpheus.width * escala;
                 Vector2 pos = { (screenWidth / 2.0f) - (imgLargura / 2.0f), 220.0f };
                 DrawTextureEx(textura_morpheus, pos, 0.0f, escala, WHITE);
@@ -242,14 +285,15 @@ int main(void)
             int fontSizeTitle = 22;
             int fontSizeContent = 34;
 
-            if (fim_de_jogo)
+            if(fim_de_jogo)
             {
                 DrawText("MORPHEUS determinou...", screenWidth/2 - MeasureText("MORPHEUS determinou...", fontSizeTitle)/2, textY, fontSizeTitle, WHITE);
                 int nameWidth = MeasureText(noAtual->conteudo, fontSizeContent);
                 int nameX = screenWidth/2 - nameWidth/2;
                 DrawText(noAtual->conteudo, nameX, textY + 40, fontSizeContent, PALETTE_BLUE);
                 
-                if (textura_morpheus.id > 0) {
+                if(textura_morpheus.id > 0) 
+                {
                     float imgLargura = textura_morpheus.width * escala;
                     Vector2 pos = { (screenWidth / 2.0f) - (imgLargura / 2.0f), 220.0f };
                     DrawTextureEx(textura_morpheus, pos, 0.0f, escala, WHITE);
@@ -257,7 +301,7 @@ int main(void)
 
                 DrawButton((Rectangle){ screenWidth/2 - 125, 580, 250, 60 }, "Jogar Novamente", 24, PALETTE_YELLOW, PALETTE_DARKRED);
             }
-            else if (noAtual->eh_personagem == 'N')
+            else if(noAtual->eh_personagem == 'N')
             {
                 char titleBuf[32];
                 sprintf(titleBuf, "Pergunta %d de %d:", pergunta_contador+1, total_perguntas);
@@ -267,7 +311,8 @@ int main(void)
                 
                 // Puxa a imagem correta baseada no número da pergunta atual
                 Texture2D textura_atual = texturas_perguntas[pergunta_contador];
-                if (textura_atual.id > 0) {
+                if(textura_atual.id > 0) 
+                {
                     float imgLargura = textura_atual.width * escala;
                     Vector2 pos = { (screenWidth / 2.0f) - (imgLargura / 2.0f), 220.0f };
                     DrawTextureEx(textura_atual, pos, 0.0f, escala, WHITE);
@@ -288,6 +333,8 @@ int main(void)
         UnloadTexture(texturas_perguntas[i]);
     }
     
+    UnloadMusicStream(tema);
+    CloseAudioDevice();
     libera_arvore(raiz); 
     CloseWindow();
 
